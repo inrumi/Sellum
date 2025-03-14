08b23991f8a260149132418fff8b5352af5c79de
Johnn Mis <inrumi@gmail.com>
2025-03-14 01:23:54 +0100

    forgot to remove this

--- keymap.c
+++ keymap.c
@@ -46,7 +46,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         XXXXXXX, KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,    /**/  KC_J,    KC_L,    KC_U,    KC_Y,    KC_QUOT,  XXXXXXX,
         XXXXXXX, KC_A,    KC_R,    KC_S,    KC_T,    KC_G,    /**/  KC_M,    KC_N,    KC_E,    KC_I,    KC_O,     XXXXXXX,
         XXXXXXX, KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    /**/  KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SCLN,  XXXXXXX,
-                                   LA_NAV,  KC_LSFT, LA_FUN,  /**/  KC_ENT, KC_SPC,  LA_SYM
+                                   LA_NAV,  KC_LSFT, LA_FUN,  /**/  _______, KC_SPC,  LA_SYM
     ),
 
     [SYM] = LAYOUT_split_3x6_3(
